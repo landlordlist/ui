dd7fdfc7fa8b1608d00ce4bcfb7462ed32dc514a
hallcode <-Fullwell33->
2020-10-07 16:56:16 +0100

    Added infinate scroll

---
+++ src/components/BalanceBox.vue
@@ -0,0 +1,195 @@
+<template>
+    <section id="balance" class="ll container">
+    <article class="section">
+        <div class="card">
+            <header class="card-header">
+                <h2 class="card-title h3">Balance</h2>
+                <ul class="tab">
+                    <li :class="`tab-item ${isOverall ? 'active' : ''}`">
+                        <a @click.prevent="isOverall=true" href="#">Overall</a>
+                    </li>
+                    <li :class="`tab-item ${isOverall ? '' : 'active'}`">
+                        <a @click.prevent="isOverall=false" href="#" class="">By Party</a>
+                    </li>
+                </ul>
+            </header>
+            <section class="card-body" v-if="isOverall">
+                <section>
+                    <p class="text-primary h5">Landlords ({{ landlordCount }})</p>
+                    <div class="grid">
+                        <div class="dot ll" v-for="i in landlordCount" v-bind:key="i"></div>
+                    </div>
+                </section>
+                <section>
+                    <p class="text-gray h5">Not landlords ({{ membersLessLandlords }})</p>
+                    <div class="grid">
+                        <div class="dot" v-for="i in membersLessLandlords" v-bind:key="i"></div>
+                    </div>
+                </section>
+            </section>
+            <section class="card-body" v-if="!isOverall">
+                <template v-for="(party, i) in parties" v-bind:key="i">
+                    <section :style="`flex-basis: ${(party.members/memberCount)*100}%`">
+                        <p class="h5">{{ party.abbr || 'None' }}</p>
+                        <div class="grid">
+                            <div class="dot ll" v-for="i in party.landlords" v-bind:key="i" :style="`background-color: #${party.colour}`"></div>
+                            <div class="dot" v-for="i in (party.members-party.landlords)" v-bind:key="i" :style="`border-color: #${party.colour}`"></div>
+                        </div>
+                    </section>
+                </template>
+            </section>
+            <div class="card-footer">
+                <ul>
+                    <li>
+                        <div class="dot ll"></div> Landlord
+                    </li>
+                    <li>
+                        <div class="dot"></div> Not a landlord
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </article>
+</section>
+</template>
+
+<script>
+export default {
+    name: 'BalanceBox',
+    props: [
+        'house'
+    ],
+    data() {
+        return {
+            isOverall: false
+        }
+    },
+    computed: {
+        membersLessLandlords() {
+            return this.memberCount - this.landlordCount
+        },
+        landlordCount() {
+            if (this.house.counts != undefined) {
+                return this.house.counts.landlords
+            }
+
+            return 0
+        },
+        memberCount() {
+            if (this.house.counts != undefined) {
+                return this.house.counts.members
+            }
+
+            return 0
+        },
+        parties() {
+            let output_array = []
+            let other = {
+                "_id": 0,
+                "abbr": "Other",
+                "colour": "C0C0C0",
+                "landlords": 0,
+                "members": 0,
+                "name": "Other"
+            }
+
+            if (this.house.party_balance == undefined) {
+                return []
+            }
+
+            for (var i = 0; i < this.house.party_balance.length; i++) {
+                let party = this.house.party_balance[i]
+                if (party.members > 10) {
+                    output_array.push(party)
+                } else {
+                    other.landlords += party.landlords
+                    other.members += party.members
+                }
+            }
+
+            output_array.push(other)
+
+            return output_array
+        }
+    },
+}
+</script>
+
+
+<style lang="scss" scoped>
+#balance {
+    padding-top: .88rem;
+
+    .card-body {
+        display: flex;
+        align-items: flex-start;
+
+        section {
+            flex: 1 1 auto;
+        }
+    }
+
+    .h5 {
+        margin: 0;
+        padding-right: .33rem;
+    }
+
+    .grid {
+        display: grid;
+        grid-template-columns: repeat(auto-fit, minmax(8px, 1fr));
+        margin-right: .22rem;
+        gap: 3px;
+    }
+
+    .dot {
+        border: 2px solid #bcc3ce;
+        background-color: #fff;
+        height: 8px;
+        width: 8px;
+        border-radius: 100%;
+
+        &.ll {
+            background-color: salmon;
+            border-color: transparent;
+        }
+    }
+
+    .card-footer ul {
+        display: flex;
+        align-items: center;
+        list-style-type: none;
+        margin: 0;
+        padding: 0;
+
+        li {
+            color: #a5a9b0;
+            display: flex;
+            align-items: center;
+            margin: 0;
+            padding: 0 .44rem 0 0;
+
+            div.dot {
+                margin: 0 .33rem 0 0;
+                border-color: #a5a9b0;
+
+                &.ll {
+                    background-color: #a5a9b0;
+                }
+            }
+        }
+    }
+
+    @media screen and (max-width: 650px) {
+        .grid {
+            grid-template-columns: repeat(auto-fit, minmax(4px, 1fr));
+            gap: 1px;
+        }
+
+        .dot {
+            height: 5px;
+            width: 5px;
+            border: 1px solid #bcc3ce;
+        }
+    }
+}
+</style>
--- src/views/List.vue
+++ src/views/List.vue
@@ -12,58 +12,7 @@
         <h1>House of {{ house.name }} </h1>
     </article>
 </section>
-<section id="balance" class="ll container">
-    <article class="section">
-        <div class="card">
-            <header class="card-header">
-                <h2 class="card-title h3">Balance</h2>
-                <ul class="tab">
-                    <li :class="`tab-item ${isOverall ? 'active' : ''}`">
-                        <a @click.prevent="isOverall=true" href="#">Overall</a>
-                    </li>
-                    <li :class="`tab-item ${isOverall ? '' : 'active'}`">
-                        <a @click.prevent="isOverall=false" href="#" class="">By Party</a>
-                    </li>
-                </ul>
-            </header>
-            <section class="card-body" v-if="isOverall">
-                <section>
-                    <p class="text-primary h5">Landlords ({{ landlordCount }})</p>
-                    <div class="grid">
-                        <div class="dot ll" v-for="i in landlordCount" v-bind:key="i"></div>
-                    </div>
-                </section>
-                <section>
-                    <p class="text-gray h5">Not landlords ({{ membersLessLandlords }})</p>
-                    <div class="grid">
-                        <div class="dot" v-for="i in membersLessLandlords" v-bind:key="i"></div>
-                    </div>
-                </section>
-            </section>
-            <section class="card-body" v-if="!isOverall">
-                <template v-for="(party, i) in parties" v-bind:key="i">
-                    <section :style="`flex-basis: ${(party.members/memberCount)*100}%`">
-                        <p class="h5">{{ party.abbr || 'None' }}</p>
-                        <div class="grid">
-                            <div class="dot ll" v-for="i in party.landlords" v-bind:key="i" :style="`background-color: #${party.colour}`"></div>
-                            <div class="dot" v-for="i in (party.members-party.landlords)" v-bind:key="i" :style="`border-color: #${party.colour}`"></div>
-                        </div>
-                    </section>
-                </template>
-            </section>
-            <div class="card-footer">
-                <ul>
-                    <li>
-                        <div class="dot ll"></div> Landlord
-                    </li>
-                    <li>
-                        <div class="dot"></div> Not a landlord
-                    </li>
-                </ul>
-            </div>
-        </div>
-    </article>
-</section>
+<BalanceBox :house="house"></BalanceBox>
 <main class="home ll container">
     <article class="section">
         <section id="members-list" :class="(isLoading ? 'loading loading-lg' : '')">
@@ -71,80 +20,32 @@
                 <PersonBox :member="member"></PersonBox>
             </template>
         </section>
-        <button class="btn btn-primary" @click="getMoreMembers()">More</button>
     </article>
 </main>
 </template>
 
 <script>
 import PersonBox from '@/components/PersonBox.vue'
+import BalanceBox from '@/components/BalanceBox.vue'
 
 export default {
     name: 'List',
     components: {
-        PersonBox
+        PersonBox,
+        BalanceBox
     },
     data() {
         return {
             house: {},
             members: [],
-            isLoading: false,
-            isOverall: false
+            isLoading: false
         }
     },
-    computed: {
+    methods: {
         membersOffset() {
             // SQL OFFSET is 0 indexed, .length is 1 indexed
             return this.members.length
         },
-        membersLessLandlords() {
-            return this.memberCount - this.landlordCount
-        },
-        landlordCount() {
-            if (this.house.counts != undefined) {
-                return this.house.counts.landlords
-            }
-
-            return 0
-        },
-        memberCount() {
-            if (this.house.counts != undefined) {
-                return this.house.counts.members
-            }
-
-            return 0
-        },
-        parties() {
-            let output_array = []
-            let other = {
-                "_id": 0,
-                "abbr": "Other",
-                "colour": "C0C0C0",
-                "landlords": 0,
-                "members": 0,
-                "name": "Other"
-            }
-
-            if (this.house.party_balance == undefined) {
-                return []
-            }
-
-            for (var i = 0; i < this.house.party_balance.length; i++) {
-                let party = this.house.party_balance[i]
-                if (party.members > 10) {
-                    output_array.push(party)
-                } else {
-                    other.landlords += party.landlords
-                    other.members += party.members
-                }
-            }
-
-            output_array.push(other)
-
-            return output_array
-        }
-    },
-    methods: {
         async getHouse() {
             let r = await fetch(`https://landlordlist.uk/api/houses/${this.$route.params.name}`)
             let data = await r.json()
@@ -159,10 +60,19 @@ export default {
         },
         async getMoreMembers() {
             this.isLoading = true
-            let r = await fetch(`https://landlordlist.uk/api/people?house=${this.$route.params.name}&offset=${this.membersOffset}`)
+            let r = await fetch(`https://landlordlist.uk/api/people?house=${this.$route.params.name}&offset=${this.membersOffset()}`)
             let data = await r.json()
             this.members = this.members.concat(data)
             this.isLoading = false
+        },
+        scroll() {
+            window.onscroll = () => {
+                let trigger = document.documentElement.scrollTop + window.innerHeight > (document.documentElement.offsetHeight - 100);
+
+                if (trigger) {
+                    this.getMoreMembers()
+                }
+            }
         }
     },
     beforeRouteUpdate(to, from, next) {
@@ -173,6 +83,9 @@ export default {
     beforeMount() {
         this.getHouse()
         this.getMembers()
+    },
+    mounted() {
+        this.scroll()
     }
 }
 </script>
@@ -192,82 +105,6 @@ export default {
         align-items: center;
     }
 
-}
-
-#balance {
-    padding-top: .88rem;
-
-    .card-body {
-        display: flex;
-        align-items: flex-start;
-
-        section {
-            flex: 1 1 auto;
-        }
-    }
-
-    .h5 {
-        margin: 0;
-        padding-right: .33rem;
-    }
-
-    .grid {
-        display: grid;
-        grid-template-columns: repeat(auto-fit, minmax(8px, 1fr));
-        margin-right: .22rem;
-        gap: 3px;
-    }
-
-    .dot {
-        border: 2px solid #bcc3ce;
-        background-color: #fff;
-        height: 8px;
-        width: 8px;
-        border-radius: 100%;
-
-        &.ll {
-            background-color: salmon;
-            border-color: transparent;
-        }
-    }
-
-    .card-footer ul {
-        display: flex;
-        align-items: center;
-        list-style-type: none;
-        margin: 0;
-        padding: 0;
-
-        li {
-            color: #a5a9b0;
-            display: flex;
-            align-items: center;
-            margin: 0;
-            padding: 0 .44rem 0 0;
-
-            div.dot {
-                margin: 0 .33rem 0 0;
-                border-color: #a5a9b0;
-
-                &.ll {
-                    background-color: #a5a9b0;
-                }
-            }
-        }
-    }
-
-    @media screen and (max-width: 650px) {
-        .grid {
-            grid-template-columns: repeat(auto-fit, minmax(4px, 1fr));
-            gap: 1px;
-        }
-
-        .dot {
-            height: 5px;
-            width: 5px;
-            border: 1px solid #bcc3ce;
-        }
-    }
 }
 
 #members-list {
